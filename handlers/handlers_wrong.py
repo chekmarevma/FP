eca762a572ba5dbe669db9a2ec0b4d6e0c7ca7e5
Maxim Chekmarev <max.chek13@gmail.com>
2023-05-07 20:51:59 +0300

    Изменен хэндлер с ошибками

--- handlers/handlers_wrong.py
+++ handlers/handlers_wrong.py
@@ -11,12 +11,13 @@ router: Router = Router()
 # Хэндлер на неправильный воод формата результата
 @router.message(StateFilter(FSM.fill_otzhim, FSM.fill_100m, FSM.fill_60m, FSM.fill_10x10, FSM.fill_swim100m, FSM.fill_podt,
                             FSM.fill_podem, FSM.fill_vihod, FSM.fill_1km_before35, FSM.fill_1km_after35,
-                            FSM.fill_3km_before35, FSM.fill_3km_after35))
+                            FSM.fill_3km_before35, FSM.fill_3km_after35, FSM.fill_brevno, FSM.fill_jump3))
 async def incorrect_input(message: Message):
     await message.answer(text=f'{lex["wrong_input"]}')
 
 # Хэндлер на ненажатие кнопок в режиме нажатия кнопок
-@router.message(StateFilter(FSM.fill_speed, FSM.fill_strong, FSM.fill_endurance, FSM.fill_group, FSM.fill_mode))
+@router.message(StateFilter(FSM.fill_speed, FSM.fill_strong, FSM.fill_endurance, FSM.fill_agility, FSM.fill_VPN,
+                            FSM.fill_group, FSM.fill_mode, FSM.fill_mark))
 async def no_button_pressed(message: Message, state: FSMContext):
     current_state = await state.get_state()
     raw_state = current_state.split(':')[1]
@@ -30,12 +31,18 @@ async def no_button_pressed(message: Message, state: FSMContext):
     elif raw_state == 'fill_endurance':
         await message.answer(text=lex['choose_type'], reply_markup=keyboard_endurance)
         await state.set_state(FSM.fill_endurance)
+    elif raw_state == 'fill_agility':
+        await message.answer(text=lex['choose_type'], reply_markup=keyboard_agility)
+        await state.set_state(FSM.fill_agility)
     elif raw_state == 'fill_group':
         await message.answer(text=lex['choose_type'], reply_markup=keyboard_type)
         await state.set_state(FSM.fill_group)
     elif raw_state == 'fill_mode':
         await message.answer(text=lex['choose_mode'], reply_markup=keyboard_mode)
         await state.set_state(FSM.fill_mode)
+    elif raw_state == 'fill_mark':
+        await message.answer(text=lex['input_mark'], reply_markup=keyboard_mark)
+        await state.set_state(FSM.fill_mark)
 
 @router.message(StateFilter(FSM.fill_age_1km, FSM.fill_age_3km))
 async def no_age_pressed(message: Message, state: FSMContext):
